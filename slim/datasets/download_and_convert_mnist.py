f970baa361cefab91cb3252dc03a8665bc92f4f8
enrique.otero <enrique.otero@beeva.com>
2017-03-14 13:44:22 +0100

    download from mnist mirror

--- slim/datasets/download_and_convert_mnist.py
+++ slim/datasets/download_and_convert_mnist.py
@@ -37,7 +37,8 @@ import tensorflow as tf
 from datasets import dataset_utils
 
 # The URLs where the MNIST data can be downloaded.
-_DATA_URL = 'http://yann.lecun.com/exdb/mnist/'
+#_DATA_URL = 'http://yann.lecun.com/exdb/mnist/'
+_DATA_URL = 'https://github.com/petar/GoMNIST/raw/master/data/'
 _TRAIN_DATA_FILENAME = 'train-images-idx3-ubyte.gz'
 _TRAIN_LABELS_FILENAME = 'train-labels-idx1-ubyte.gz'
 _TEST_DATA_FILENAME = 't10k-images-idx3-ubyte.gz'
